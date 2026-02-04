2c5f160afbf0ee11e58508549fc67dd825ec989f
wilman <wilman01@gmail.com>
2026-02-04 10:30:04 -0400

    Initial commit: Activos Fijos project with HTML pages

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('googleSignIn').addEventListener('click', function() {
+        alert('Redirigiendo a autenticación de Google Workspace...');
+    });
+});
